6f38500805ec896d5fe4c801b525aa09619a9c78
TAYYAB <bbcblack28@gmail.com>
2021-10-03 07:37:39 +0500

    my first ruby file

--- one.rb
+++ one.rb
@@ -1,1 +1,764 @@
-print "hello word"
+#puts "TAYYAB"
+
+#print "hello word "
+
+# first code
+
+
+#puts"ko bro"
+#puts"|_______|"
+#puts"|-------|"
+#puts"|_______|"
+
+
+# enf of code 
+
+# two code
+
+
+# name="TAYYAB"
+# age="20"
+# (puts"There ones man named "+name)
+# (puts"he was "+age+" years old")
+# (puts "but he not like "+age+" ")
+# puts"--'_'--"
+
+# two end  code 
+
+
+# three cod 
+
+
+# name="TAYYAB"
+# age="20"
+
+# name="Tayyab"
+# (puts"There ones man named "+name)
+# (puts"he was "+age+" years old")
+# age="18"
+# (puts "but he not like "+age+" ")
+# puts"--'_'--"
+
+# three end of code 
+
+# the data type in ruby fore 
+
+# name = "Tayyab"
+# age= 18
+# id=9.9
+# male= true
+# femail = false
+# flou= nil
+
+# end of fore 
+
+# ---1---
+
+# name = "echo"
+
+# print name
+
+# ---1end---
+
+# ---1.1--
+
+# name = "echo"
+
+# puts name.upcase()
+    
+
+# ---1.1end---
+
+# ---1.1.2--
+
+# name = "echo"
+
+# puts name.downcase()
+    
+
+# ---1.1.2end---
+
+# ---1.1.3--
+
+# name = "echo"
+
+# puts name.strip()
+    
+
+# ---1.1.3end---
+
+# ---1.1.4--
+
+# name = "echo"
+
+# puts name.length()
+    
+
+# ---1.1.4end---
+
+# ---1.1.5--
+
+# name = "echo"
+
+# puts name.include? "e"
+    
+
+# ---1.1.5end---
+
+# ---1.1.6--
+
+# name = "echo"
+
+# puts name [0]
+    
+
+# ---1.1.6end---
+
+# ---1.1.6--
+
+# name = "echo"
+
+# puts name [0,2]
+    
+
+# ---1.1.6end---
+
+# ---1.1.7--
+
+# name = "echo"
+
+# puts name.index("e")
+    
+
+# ---1.1.7end---
+
+# ---1.1-7.0---
+
+# num = 8
+# (puts "my fav num " +  num.to_s) 
+
+# ---1.1-7.0 end---
+
+# ---1.1-7.1---
+# num = 10.001
+# puts num.abs()
+# ---1.1-7.1 end---
+
+# ---1.1-7.2---
+# num = 10.001
+# puts num.round()
+# ---1.1-7.2 end---
+
+# ---1.1-7.3---
+# num = 10.001
+# puts num.ceil()
+# ---1.1-7.3 end---
+
+# ---1.1-7.4---
+# num = 10.001
+# puts num.floor()
+# ---1.1-7.4 end---
+
+# ---1.1-7.5---
+# num = 10.001
+# puts Math.sqrt(10)
+# ---1.1-7.5 end---
+
+# ---1.1-7.6---
+# num = 10.001
+# puts Math.log(10)
+# ---1.1-7.6 end---
+
+
+# master way of work haha "_"
+
+
+# 0.0.1 start 
+
+# puts "Enter your name sir"
+# uesar = gets
+# solif = "Hello sir "+uesar+" \n how are you"
+
+# puts solif
+
+
+# 0.0.1 end
+
+# 0.0.2 start 
+
+#  puts "Enter your name sir"
+# uesar = gets.chomp()
+# solif = "Hello sir "+uesar+" \n how are you"
+# puts solif
+# 0.0.2 end
+
+
+
+# 0.0.3 start
+#   puts "Enter your name sir" 
+#  input = gets.chomp()
+#  puts "Enter your age sir" 
+#  age = gets.chomp()
+#  puts "Enter your job sir" 
+#  job = gets.chomp()
+
+#  name = "Hello sir "+input+" wow"
+#  ag = "Your age is "+age+" old"
+#  jb = "Your job is "+job+" nice"
+ 
+#  puts name
+#  puts ag
+#  puts jb
+#  puts "Thanks for ues app sir "+name+" \n eng"
+
+ # 0.0.3 end
+
+ # 0.0.4 start
+
+#  puts "Enter your number"
+#  num = gets.chomp()
+#  puts "Enter Your number"
+#  num1 = gets.chomp()
+
+#  puts (num.to_i + num1.to_i)
+
+# 0.0.4 end
+
+# 0.0.5 start
+
+# puts "Enter your number"
+# num = gets.chomp()
+# puts "Enter Your number"
+# num1 = gets.chomp()
+
+# puts (num.to_f + num1.to_f)
+
+# 0.0.5 end
+
+# 0.0.6start 
+
+# puts "Enter your name "
+# name = gets.chomp()
+# puts "Enter your age "
+# age = gets.chomp()
+# puts "Enter your job "
+# job = gets.chomp()
+
+# puts "your name is " +name
+# puts "your age  is " +age
+# puts "your job  is " +job
+
+# 0.0.6 end
+
+# 0.0.7 start
+
+# name = ["any","echo","honey","abby"]
+
+# puts name[0] 
+
+# 0.0.7 end
+
+# 0.0.8 start
+
+#  name = ["any","echo","honey","abby"]
+
+#  puts name[-1] 
+
+# 0.0.8 end
+
+# 0.0.9 start
+
+# name = ["any","echo","honey","abby"]
+
+# puts name[0,2] 
+
+# 0.0.9 end
+
+# 0.0.10 start
+
+# name = ["any","echo","honey","abby"]
+# name[3] = "Tayyab" 
+# puts name[0,4] 
+
+# 0.0.10 end
+
+
+# 0.0.10.0 start
+
+# name = Array.new
+# name[1] = "new" 
+# name[2] = "echo" 
+# name[3] = "brst" 
+# name[4] = "Tayyab" 
+# puts name[0,5] 
+
+# 0.0.10.0 end
+
+# 0.0.10.1 start
+
+# name = ["any","echo","honey","abby"]
+# name[3] = "Tayyab" 
+# puts name.length 
+
+# 0.0.10.1 end
+
+# 0.0.10.2 start
+
+# name = ["any","echo","honey","abby"]
+# name[3] = "Tayyab" 
+# puts name.include? "Tayyab" 
+
+# 0.0.10.2end
+
+# 0.0.10.3 start
+
+# status = {
+#     "pk" => "pakistan",
+#     "in" => "india",
+#     "usa" => "america",
+#     "pro" => "master",
+# }
+
+# puts status["pk"]
+# puts status["pro"]
+
+# 0.0.10.3 end
+
+# 0.0.10.4 start
+
+# def hi
+#   puts "hi bro "
+#   puts "?'-'?"
+# end   
+
+# name = hi()
+
+# puts name
+
+
+# 0.0.10.4 end
+
+
+# 0.0.10.5 start
+
+# def hi(nice)
+#   puts "hi  "+nice+" bro "
+#   puts "?'-'?"
+# end   
+
+# puts "Enter your name "
+
+# xxx = gets.chomp
+
+# # nice = xxx
+
+
+# name = hi(xxx)
+
+# puts name
+
+
+# 0.0.10.5 end
+
+
+# 0.0.10.6_start
+
+# puts "Enter your name  sir "
+# name = gets.chomp
+# puts "Enter your age  sir "
+# age = gets.chomp
+# puts "Enter your job  sir "
+# job = gets.chomp
+
+# def bbc(name =" ",age = " ",job = " ")
+#     puts "hello "+name+" sir "
+#     puts "your age is only  "+age.to_s+" years old"
+#     puts "your job is " + job 
+# end
+
+# bbc(name,age,job)
+
+# 0.0.10.6 end
+
+# 0.0.10.7
+
+# def hi(nice)
+#  return   puts "echo branch"
+#   puts "hi  "+nice+" bro "
+#   puts "?'-'?"
+# end   
+
+# puts "Enter your name "
+
+# xxx = gets.chomp
+
+# hi(xxx)
+
+# 0.0.10.7
+
+#{pro code }
+
+# 0.0.10.8 start 
+
+# ismali = false
+# hight = true
+
+# if ismali && hight
+#     puts "ok you can go now "
+    
+# elsif ismali && !hight
+#     puts "you are not big man "
+
+# elsif hight && !ismali
+#     puts "you are not  man "    
+# else
+#     puts "ok you can not go now "
+
+# end
+
+# 0.0.10.8 end 
+
+
+# 0.0.10.9 start
+
+# def myfirst(vc,vc1,vc2)
+#  if vc.to_i >= vc1.to_i && vc.to_i >= vc2.to_i
+#     puts vc
+     
+#  elsif vc1.to_i >= vc.to_i && vc1.to_i >= vc2.to_i
+#      puts vc1
+# elsif vc2.to_i >=vc.to_i && vc2.to_i >= vc1.to_i 
+#    puts vc2
+#  end
+# end
+
+# puts "Enter your number "
+# vc = gets.chomp
+# puts "Enter your number "
+# vc1 = gets.chomp
+# puts "Enter your number "
+# vc2 = gets.chomp
+
+# myfirst(vc,vc1,vc2)
+
+# 0.0.10.9 end
+
+# 0.0.10.10 start 
+
+# puts "Enter your number "
+# vc = gets.chomp.to_f
+# puts "Enter your op  number "
+# vc1 = gets.chomp
+# puts "Enter your number "
+# vc2 = gets.chomp.to_f
+
+# if vc1 == "+"
+#     puts (vc + vc2 )
+    
+# elsif vc1 == "-"
+#     puts(vc - vc2)
+# elsif vc1 == "*"
+#     puts(vc * vc2)    
+# elsif vc1 == "/"
+#     puts(vc / vc2)      
+# else
+#     puts "your press woring way"
+# end
+
+# 0.0.10.10 end
+
+
+# 0.0.11.0 start
+
+
+# def day_name(day)
+    
+# case day  
+# when "mon"
+#     day_name = "monday"    
+# when  "Tue"
+#     day_name = "Tuesday" 
+# when "wed"
+#     day_name = "wednsday" 
+# when "Thu"
+#     day_name = "Thursday"  
+# when "fri"
+#     day_name = "friday"
+# when "sat"
+#     day_name = "saturday"             
+# when "sun"    
+#     day_name = "sunday" 
+# else
+#     day_name = "invalid "
+
+# end
+#  return day_name   
+# end
+
+# puts day_name("fri")
+
+# 0.0.11.1 end
+
+
+#0.0.11.2 start 
+
+# index = 1
+# while index    <= 10 
+#     puts index
+#     puts "Tayyab"
+#     index += 1
+# end
+
+#0.0.11.2 end
+
+#0.0.11.3 start
+# word = "Tayyab"
+# gase =""
+# number = 0
+# numlim = 3
+# outeof= false
+
+# while gase != word
+#     if number < numlim && !outeof
+
+#         puts "Enter a best wort to won"
+#         gase = gets.chomp()
+#         number += 1
+#     else
+#         outeof = true
+#     end
+
+   
+
+
+# end
+# if outeof
+#     puts "you loze"    
+#  else  
+# puts "yau are very good haha"
+#  end
+#0.0.11.3 end 
+
+
+#0.0.11.4 start 
+
+# bbc = ["echo","any","lala","aren","Tayyab"]
+
+# for x in bbc
+#     puts x
+# end    
+
+# bbc.each do |x|
+#     puts x
+# end   
+
+
+#0.0.11.4 end 
+
+#0.0.11.5 start
+
+# def pow(x,y)
+# result = 1
+#  y.times do |index|
+#   result = result * x
+# end
+# return result
+# end
+
+# puts pow(5,2)
+
+#0.0.11.5 end
+#0.0.11.6 start
+# File.open("redme.txt","r")do |file| 
+# puts file.read()
+# end
+#0.0.11.6 end
+
+
+# 0.0.11.7 start
+# File.open("redme.txt","a") do |file|
+# file.write("Tayyab . bra")
+# end
+
+
+# 0.0.11.7 end
+
+# 0.0.11.8 start
+
+# begin
+#     num = 10/0
+# rescue 
+#     puts "10 is not divied by zeero "
+# end
+# 0.0.11.8 end
+
+
+# 0.0.11.9 start
+
+# class Schoo
+#     attr_accessor :school , :name , :age ,:gender , :father
+# end
+
+# books = Schoo.new()
+# books.school = "New "
+# books.name = "Tayyab"
+# books.age = 70
+# books.gender = "male"
+# books.father = "Rehmat"
+# puts books.gender
+
+# book = Schoo.new()
+# book.school = "New "
+# book.name = "Tayyab"
+# book.age = 70
+# book.gender = "female"
+# book.father = "Rehmat"
+# puts book.gender
+
+# 0.0.11.9 end
+
+# 0.0.11.10 start 
+
+# class Nec    #  (Tayyab
+#     attr_accessor :name , :fadher , :age
+
+#     def initialize(name,fadher,age)
+#         @name = name
+#         @fadher = fadher
+#         @age = age
+#     end
+
+    
+# end
+
+# x = Nec.new("Tayyab","rahmat",18)
+# y = Nec.new("moshan","rahmat",17)
+# puts x.name
+# puts y.name    # end )
+
+# 0.0.11.10 end 
+
+# 0.0.12.0 start 
+
+#class Nec    #  (Tayyab
+#     attr_accessor :name , :fadher , :age
+
+#     def initialize(name,fadher,age)
+#         @name = name
+#         @fadher = fadher
+#         @age = age
+
+#     end
+#     def has_honers
+#         if @age >= 18
+#             return true
+#         end
+#         return false
+#     end
+# end
+
+# x = Nec.new("Tayyab","rahmat",18)
+# y = Nec.new("moshan","rahmat",17)
+# puts x.name
+# puts y.name 
+# puts y.has_honers
+
+# 0.0.12.0 end
+
+# 0.0.12.0 start 
+
+# class Nec    #  (Tayyab
+#     attr_accessor :promp , :answer 
+
+#     def initialize(name,fadher,age)
+#         @name = name
+#         @fadher = fadher
+#         @age = age
+#     end
+
+    
+# end
+
+# class Question
+#     p1 = "what  color are apple? \n (a)red \n (b)black \n (c)blue "  
+#     p2 = "what color are you pick? \n (a)red \n (b)black \n (c)blue "
+#     p3 = "what color is banans \n (a)yello \n (b)black \n (c)blue "
+# end
+
+# question = [
+#     Question.new(p1, "a"),
+#     Question.new(p2, "c"),
+#     Question.new(p3, "b")
+# ]
+# def tun_test (question)
+# answer = ""
+# scoker = 0
+# for question in questions
+#     put.question.promp
+#     answer = gets.chomp()
+#     if answer == question.answer
+#         scoker+=1
+#     end
+# end
+#     puts ("you have "+scoker.to_s+"/ "+questions.length.to_s + " ko")
+# end
+
+# tun_test(questions)
+
+# 0.0.12.0 end 
+
+# 0.0.10.1 start 
+
+# class Chef
+#     def  num1
+#        puts "hi you in num1 " 
+#     end
+#     def num2
+#         puts "hi you in num2 " 
+#     end
+#     def num3
+#         puts "hi you in num3 " 
+#     end
+# end
+# class Pro_Chef < Chef
+#     def pro
+#         puts "your in pro "
+#     end
+#     def super
+#         puts "your in super "
+#     end
+# end
+# chef = Chef.new()
+# chef.num1
+# pronum = Pro_Chef.new()
+# pronum.num2
+# pronum.pro
+# pronum.super 
+
+
+# 0.0.10.1 end 
+
+# 0.0.10.2 start
+
+# module Tools
+# def info (name)
+#     puts "say hi #{name} sir"
+# end
+# def say (bay)
+#     puts "say  ko  #{name} sir"
+# end
+# end
+
+# include  Tools 
+# Tools.info("Tayyab")
+
+#0.0.10.3 end 
+    
+# next lifle 
+
+#require_relative "one.rb"
+
+# include Tools 
+
+# ko 
+
