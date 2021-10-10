65b5dbdd6a69912d11d3e28e66a0a3e2fcf0ffcd
TAYYAB <bbcblack28@gmail.com>
2021-10-10 07:10:49 +0500

    new commit in ruby bro 2

--- two.rb
+++ two.rb
@@ -1,81 +1,277 @@
-class Car 
-
-box = 5
-new = 3 
-old = 2
-
-def accelerate
-
-
-end
-
-def start
-puts "new"  
-end
-
-def stop
-
-puts "stop"
-end
+# class Car
 
 
 
+# box = 5
 
+# new = 3
 
-end
-
-bay = Car.new
-x1= Car.new
-bay.start
-x1.stop
-
-
-x = x 
-
-puts x.class
-
-class Super < Car
-
-  def spped
-    puts "1000"
-  end
-
-end
-
-x2 = Super.new
-x3 = Super.new
-
-x2.stop
-x3.start
-x3.spped
-
-
-class Pro 
-$master = "Tayyab"
-#localmaster2 = "tayyab"
-@@class_variable = "pro"
-@instance_variable = "pro 1"
-CONSTANT = "pro my"
-
-def pic
-puts  $master
-#puts  localmaster2
-puts  @@class_variable
-puts  @instance_variable
-puts  CONSTANT
+# old = 2
 
 
 
-end
-
-
-end
+# def accelerate
 
 
 
-
-x0 = Pro.new
-x0.pic
+# end
 
 
+
+# def start
+
+# puts "new"
+
+# end
+
+
+
+# def stop
+
+
+
+# puts "stop"
+
+# end
+
+
+
+# end
+
+
+
+# bay = Car.new
+
+# x1= Car.new
+
+# bay.start
+
+# x1.stop
+
+
+
+# x = x
+
+
+
+# puts x.class
+
+
+
+# class Super < Car
+
+
+
+#   def spped
+
+#     puts "1000"
+
+#   end
+
+
+
+# end
+
+
+
+# x2 = Super.new
+
+# x3 = Super.new
+
+
+
+# x2.stop
+
+# x3.start
+
+# x3.spped
+
+
+
+# class Pro
+
+# $master = "Tayyab"
+
+# #localmaster2 = "tayyab"
+
+# @@class_variable = "pro"
+
+# @instance_variable = "pro 1"
+
+# CONSTANT = "pro my"
+
+
+
+# def pic
+
+# puts  $master
+
+# #puts  localmaster2
+
+# puts  @@class_variable
+
+# puts  @instance_variable
+
+# puts  CONSTANT
+
+
+
+# end
+
+
+
+# end
+
+
+
+# x0 = Pro.new
+
+# x0.pic
+
+
+
+# x = 200
+
+# if !(x == 200)
+
+#   puts "x is not 200"
+
+# else
+
+#   puts "x is #{x} hahaha"
+
+# end
+
+
+
+# x = 100
+
+# y = 5
+
+# z = 15
+
+
+
+# if x > y && x > z
+
+#   puts "x is big #{x}"
+
+# elsif y > x && y > z
+
+#   puts "x is big #{y}"
+
+# else
+
+#   puts "x is big #{z}"
+
+# end
+
+# puts y > 10 ? "y is #{y} big than 10 " : "y is #{y} somill than 10"
+
+
+
+# x = 20
+
+# case x 
+
+# when   20
+# puts "x  is #{x} ok boss"
+# when  30 
+#   puts "x is #{x} ok boss"
+
+# when  40 
+#   puts "x is #{x} ok boss"
+
+# when  50 
+#   puts "x is #{x} ok boss"
+
+# else  10
+#   puts "x is #{x} ok boss"
+
+# end         
+
+
+
+# i = 1 
+# while i <= 10
+#   puts "i is #{i}"
+#    i = i + 1
+# end
+
+
+
+# i = 1 
+# until i == 10
+#   puts "i is #{i}"
+#    i = i + 1
+# end
+
+# x = (1..20)
+# for i in x
+#   puts "your x is #{i}"
+# end
+
+# x = (1..20)
+# x = ["tayyab","best","pro", "master"]
+# for i in x
+#   puts "your x is #{i}"
+# end
+
+
+#  xxx = ["tayyab","best","pro", "master"]
+#  xxx.each do |x|
+#   puts "your x is have #{x}"
+#  end
+
+  # xxx = ["tayyab","best","pro", "master"]
+
+  #  xxx.each{|x| puts "you have #{x}"}
+
+  # 1.upto(10) do |x|
+  #   puts x
+  # end
+
+  
+  # "A".upto("Z" ) do |x|
+  #   puts x
+  # end
+
+  # best  loop 
+
+  # 10.downto(1) do |x|
+  #   puts "x is #{x}" 
+  # end
+# end of best loop 
+
+# x = 1 
+# 10.times   do 
+#   puts "Tayyab is #{x}"
+#    x = x + 1 
+# end
+
+# x = 1
+# loop do 
+#   puts "Tayyab is #{x}"
+#     x = x + 1 
+# end
+
+
+# x = 1
+# loop do 
+#   break if x == 5001 
+#   puts "Tayyab is #{x}"
+#     x +=1
+# end
+
+# end of all loop it is good ha ha 
+
+# x = "Tayyab"
+# yau = '  new'
+
+# puts x.length
+# puts yau.size
+# puts x.methods
+
+# a = [1,2,3,4,5,6,7,8,9,]
+# puts a[-1] 
 
